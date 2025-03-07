ee45242f99fd29f72662d12c04a1809c8d85baff
Grinyaka <grinyaka13@gmail.com>
2025-03-07 15:21:27 +0500

    Deploy is borked

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,9 @@ import sql from './db'
 import 'dotenv/config'
 import { handleRequest } from './controllers'
 
-const server = Bun.serve({
+Bun.serve({
   port: process.env.PORT || 3000,
   async fetch(req) {
     return handleRequest(sql, req)
   },
 })
-
-console.log(`Server is running on http://localhost:${server.port}`)
